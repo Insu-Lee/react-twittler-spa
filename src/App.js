e52cc4aec68d2ad392e7c84e667a232d517e4bcf
Insu-Lee <insu.lee@mrhi.or.kr>
2025-02-28 16:59:28 +0900

    reference

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
 import './App.css';
 // TODO - react-router-dom을 설치 후, import 구문을 이용하여 BrowserRouter, Routes, Route 컴포넌트를 불러오세요.
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Sidebar from './Sidebar';
 import Tweets from './Pages/Tweets';
 // TODO - import문을 이용하여 MyPage, About 컴포넌트를 불러오세요.
+import MyPage from './Pages/MyPage';
+import About from './Pages/About';
 
 const App = () => {
   return (
     <div>
       {/* TODO - BrowserRouter 컴포넌트를 작성합니다. */}
-      <div className="App">
-        <main>
-          <Sidebar />
-          <section className="features">
-            {/* TODO - Route 컴포넌트를 이용하여 경로(path)를 설정하고 Tweets, Mypage, About 컴포넌트를 연결합니다. */}
-            <Tweets />
-          </section>
-        </main>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <main>
+            <Sidebar />
+            <section className="features">
+              {/* TODO - Route 컴포넌트를 이용하여 경로(path)를 설정하고 Tweets, Mypage, About 컴포넌트를 연결합니다. */}
+              <Routes>
+                {/* 경로는 path로 컴포넌트는 element로 연결해 줍니다. */}
+                <Route path="/" element={<Tweets />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/mypage" element={<MyPage />} />
+              </Routes>
+            </section>
+          </main>
+        </div>
+      </BrowserRouter>
     </div>
   );
 };
